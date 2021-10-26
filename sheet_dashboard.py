178626ecda87c50b5fb57441331c739b4221ebe3
Momo Ueda <umomo88@gmail.com>
2021-10-25 22:01:37 -0700

    updated config file headings

--- sheet_dashboard.py
+++ sheet_dashboard.py
@@ -11,8 +11,7 @@ config.read('config.cfg')
 
 eventbrite_token = config['user_info']['eventbrite_token']
 event_id = config['eventbrite_ids']['event_id']#update event_id for each event
-event_name = 'R&D'
-
+event_name = 'DataScience'
 trello_key = config['user_info']['trello_key']
 trello_token = config['user_info']['trello_token']
 card_id = config['trello_ids']['card_id']#update card_id for each trello card
@@ -26,11 +25,11 @@ attendees = requests.get( "https://www.eventbriteapi.com/v3/events/" +event_id+ 
 
 pagination = attendees.json()['pagination']
 count = pagination['object_count'] #get total number of orders
-
+print(count)
 #get current date and time to be included in sheets
 from datetime import date
 today = date.today().strftime('%Y-%m-%d')
-print(today)
+
 #----sheets
 #leave this untouched
 #define gsheet_api_check to obtain permission
